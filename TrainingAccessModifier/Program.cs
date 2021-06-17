152b12c1d363219247fec5ec28a902f8606ed88d
Kosmas Suryopranoto <kosmas@formulatrix.com>
2021-06-17 18:52:11 +0700

    renew

---
+++ TrainingAccessModifier/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TrainingAccessModifier
+{
+  class RunVertex
+  {
+    internal RunVertex(float a, float b, float c)
+    {
+      VertexQuadratic V1 = new VertexQuadratic(a, b, c);
+      float x = VertexQuadratic.vertexPoint[0];
+      float y = VertexQuadratic.vertexPoint[1];
+      Console.WriteLine("Vertex Point coordinate: " + x.ToString() + " , " + y.ToString());
+    }
+  }
+
+  class RunSplitingSentences
+  {
+    internal RunSplitingSentences(string sentence)
+    {
+      SplittingSentences S1 = new SplittingSentences(sentence);
+      Console.WriteLine(S1.Words[1]);
+      S1.Words[1] = "Java";
+      Console.WriteLine(S1.Words[1]);
+    }
+  }
+
+  public class Run
+  {
+    public Run()
+    {
+      RunVertex V = new RunVertex(a: 1.1F, b: 0.2F, c: 1.5F);
+
+      RunSplitingSentences T = new RunSplitingSentences(
+        sentence: "Learning C# Language From Scratch");
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      RunVertex V = new RunVertex(a: 1.1F, b: 0.2F, c: 1.5F);
+
+      RunSplitingSentences T = new RunSplitingSentences(
+        sentence: "Learning C# Language From Scratch");
+    }
+  }
+}
+
+// namespace Vertex{
+//   class runVertex{
+//     static void  countVertexPoint(){
+//       VertexQuadratic V1 = new VertexQuadratic(1, 1, 1);
+//     }
+    
+    
+//   }
+// }
---
+++ TrainingAccessModifier/vertexPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TrainingAccessModifier{
+  class VertexQuadratic{
+    internal static float[] vertexPoint;
+    private float h = 0F;
+    private float k = 0F;  
+    internal VertexQuadratic(float a, float b, float c){
+      h = -b / (2*a);
+      k = a * h * h + b * h + c;
+
+      // Console.Write(h);
+      // Console.Write(" ");
+      // Console.WriteLine(k);
+
+      vertexPoint = new float[]{h, k};
+    }
+  }
+}
